92c7efa8a674c15e3d213fc1c5f253330e25b039
Apress <gwenanspearing@apress.com>
2016-10-18 14:16:34 +0100

    First commit

---
+++ 9781484200179_Scripts_Chapter_01/9781484200179_Scripts_Chapter_01/Chapter1-Scripts-1.ps1
@@ -0,0 +1,13 @@
+﻿param (
+    $InitialValue,
+    $ConditionValue
+)
+
+$i = $InitialValue
+While ($i -lt $ConditionValue) {
+    "Value is $i"
+    $i += 1
+    if ($i % 5 -eq 0) {
+        Break
+    }
+}
